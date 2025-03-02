9bc74d90ca30f5542b51639157ec87f0fb41a525
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2025-03-02 13:20:21 +0100

    added collisions to the world

--- scripts/minimap.gd
+++ scripts/minimap.gd
@@ -8,35 +8,47 @@ extends CanvasLayer
 var player_node: Node2D
 
 func _ready() -> void:
-	if owner == null:
-		print_error("Owner is null. Cannot access 'TileMap'.")
-		return
-	
-	var tilemap_node = owner.get_node("TileMap")
-	if tilemap_node == null:
-		print_error("TileMap node is not found.")
-		return
-	
-	for tilemap in tilemap_node.get_children():
-		if tilemap is TileMap:
-			var minimap_tilemap = tilemap.duplicate()
-			setup_minimap(minimap_tilemap)
-			
-			if tilemap.name == "Ground":
-				var used_rect: Rect2i = tilemap.get_used_rect()
-				set_minimap_limits(used_rect)
+	print("Minimap Owner: ", owner)
+	call_deferred("_initialize_minimap")
+
+func _initialize_minimap() -> void:
+	if owner:
+		var tilemap_parent = owner.find_child("TileMap", true, false)
+		
+		if tilemap_parent:
+			print("Found TileMap Parent: ", tilemap_parent.name)
+
+			for tilemap in tilemap_parent.get_children():
+				if tilemap is TileMap:
+					var minimap_tilemap = tilemap.duplicate()
+					setup_minimap(minimap_tilemap)
+
+					if tilemap.name == "Ground":
+						var used_rect: Rect2i = tilemap.get_used_rect()
+						set_minimap_limits(used_rect)
+		else:
+			print("TileMap node not found under owner!")
+	else:
+		print("Owner is still null!")
 
 func _process(delta: float) -> void:
 	if player_node:
-		Minimap_camera.global_position = lerp(Minimap_camera.global_position, player_node.global_position, 0.2)
+		Minimap_camera.global_position = Minimap_camera.global_position.lerp(player_node.global_position, 5 * delta)
 		player_marker.global_position = player_node.global_position
+	else:
+		if owner:
+			player_node = owner.get_node_or_null("Player")
+			if player_node:
+				print("Player node found: ", player_node.name)
+			else:
+				print("Player node not found!")
 
-func setup_minimap(minimap_tilemap: TileMapLayer) -> void:
+func setup_minimap(minimap_tilemap: TileMap) -> void:
 	sub_viewport.add_child(minimap_tilemap)
 
 func set_minimap_limits(used_rect: Rect2i) -> void:
-	Minimap_camera.limit_left = used_rect.position.x * 16
-	Minimap_camera.limit_top = used_rect.position.y * 16
-	Minimap_camera.limit_right = (used_rect.position.x + used_rect.size.x) * 16
-	Minimap_camera.limit_bottom = (used_rect.position.y + used_rect.size.y) * 16
-	
+	var tile_size = 16  # Adjust if your tile size is different
+	Minimap_camera.limit_left = used_rect.position.x * tile_size
+	Minimap_camera.limit_top = used_rect.position.y * tile_size
+	Minimap_camera.limit_right = (used_rect.position.x + used_rect.size.x) * tile_size
+	Minimap_camera.limit_bottom = (used_rect.position.y + used_rect.size.y) * tile_size
